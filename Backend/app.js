c9f140c9fe9bf6e0f34c6bfdd290c5361ff1f16f
Yuvraj <ys2554374@gmail.com>
2025-04-18 14:28:31 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const connecToDb = require('./db/db');
 app.use(cors());
 
+
+connecToDb();
 app.get('/', (req, res) => {
     res.send('hELLO');
 });
